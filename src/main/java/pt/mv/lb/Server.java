a9083485df45c483ebc58b6d669b1dabf1332eb9
Micael Vicente <micaelvicente@gmail.com>
2025-01-20 14:13:53 +0000

    feat: simple load balancer

---
+++ src/main/java/pt/mv/lb/Server.java
@@ -0,0 +1,18 @@
+package pt.mv.lb;
+
+import java.util.Objects;
+
+public record Server(String address) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Server server = (Server) o;
+        return Objects.equals(address, server.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(address);
+    }
+}
